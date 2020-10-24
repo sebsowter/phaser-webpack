892d4b53551142edba30e0efd0ca893ab21c7c21
sebsowter <seb@sebeynott.com>
2020-10-24 15:36:52 +0100

    TypeScript and refactor

--- src/js/MarioScene.ts
+++ src/js/MarioScene.ts
@@ -10,16 +10,13 @@ export default class MarioScene extends Phaser.Scene {
   }
 
   public create(): void {
-    // Create tilemap
     const tilemap = this.make.tilemap({
       key: "tilemap",
     });
-
     const tileset = tilemap.addTilesetImage("tiles");
-
     const layer = tilemap.createDynamicLayer(0, tileset, 0, 0);
-    layer.setCollision(6);
 
+    layer.setCollision(6);
     layer.forEachTile((tile) => {
       switch (tile.index) {
         case 2:
@@ -28,23 +25,17 @@ export default class MarioScene extends Phaser.Scene {
       }
     });
 
-    // Create Mario
     const mario = new MarioSprite(this, 32, 192, "player");
 
-    // Set world bounds
     this.physics.world.setBounds(
       0,
       0,
       tilemap.widthInPixels,
       tilemap.heightInPixels
     );
-
     this.physics.world.TILE_BIAS = 12;
-
-    // Add collider between Mario and tilemap layer
     this.physics.add.collider(mario, layer);
 
-    // Set camera bounds
     this.cameras.main.setBounds(
       0,
       0,
